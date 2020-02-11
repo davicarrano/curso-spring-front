81812512f2daf6b564f74e7eed1fdbdbaee48f02
davicarrano <davicarrano@yahoo.com.br>
2020-02-11 16:18:39 +0000

    carregando as paginas via lazy load

---
+++ src/pages/list/list.module.ts
@@ -0,0 +1,13 @@
+import{IonicPageModule} from 'ionic-angular/module';
+import {NgModule} from '@angular/core';
+import {ListPage} from './list';
+
+@NgModule({
+    declarations: [ListPage],
+    imports: [IonicPageModule.forChild(ListPage)]
+    
+})
+
+export class HomeModule{
+
+}
