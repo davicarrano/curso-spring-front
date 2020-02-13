0fdf851381f404d17c0ed1a3872ee2e294201a63
davi carrano <davicarrano@yahoo.com.br>
2020-02-13 16:37:38 -0300

    gerei o intercept de erro e fiz o bind dos dados de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { CategoriaService } from '../services/domain/categoria.service';
 import { ProdutoService } from '../services/domain/produto.service';
+import { ErrorInterceptorProvider } from '../interceptors/error-interceptor';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { ProdutoService } from '../services/domain/produto.service';
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     CategoriaService,
-    ProdutoService
+    ProdutoService,
+    ErrorInterceptorProvider
   ]
 })
 export class AppModule {}
